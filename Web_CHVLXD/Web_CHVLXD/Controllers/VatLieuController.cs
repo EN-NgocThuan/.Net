1f890d1d664ab040c9072cc3f13fa5fa80956e97
EN-NgocThuan <135415371+EN-NgocThuan@users.noreply.github.com>
2023-10-05 18:04:52 +0700

    first commit

---
+++ Web_CHVLXD/Web_CHVLXD/Controllers/VatLieuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_CHVLXD.Models;
+namespace Web_CHVLXD.Controllers
+{
+    public class VatLieuController : Controller
+    {
+        // GET: VatLieu
+        QL_VLXDDataContext db = new QL_VLXDDataContext();
+        public ActionResult Items()
+        {
+            var lstItem = db.VATLIEUs.Select(r=>r); 
+            return View(lstItem);
+        }
+    }
+}
---
+++ Web_CHVLXD/Web_CHVLXD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_CHVLXD.MvcApplication" Language="C#" %>
